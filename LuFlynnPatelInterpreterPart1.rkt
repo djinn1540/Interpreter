1589cc7a133d964221fcb065a602c4473a1cf79d
djinn1540 <gregoryjamesflynn@gmail.com>
2018-03-05 13:21:59 -0500

    code that we submitted for part 1

--- LuFlynnPatelInterpreterPart1.rkt
+++ LuFlynnPatelInterpreterPart1.rkt
@@ -4,11 +4,11 @@
 ;Import Parser
 (require "simpleparser.scm")
 
-;Interpret takes in a textfile and returns the result
+;Interpret takes in a textfile and returns the value/bool mapped to 'return as the result of the returned m_state of the parsetree
 ;Start
 (define interpret
   (lambda (filename)
-    (m_state (parser filename) emptystate)))
+    (s_find 'return (m_state (parser filename) emptystate))))
 
 ;The empty state is defined as a list of two empty lists
 (define emptystate '(()()))
@@ -46,31 +46,24 @@ Relevant abstractions are left below the functions that have them, otherwise gen
 ;Check the prefix from the parsetree standpoint to know what to apply to the statement
 (define stmttype caar)
 ;Check this particular expressions' prefix/operator
-(define prefix (lambda (expr)
-                 (cond
-                   ((pair? expr) (car lis))
-                   (else expr))))
+(define prefix car)
 
 ;List of arithmatic operators
 (define arithop '(+ - / * %))
 ;List of declare and assign operators
 (define declop '(var =))
 ;List of boolean operators
-(define boolop '(== != >= <= > <))
+(define boolop '(== != >= <= > < || && !))
 ;List of if and while operators
 (define ifwhileop '(if while))
 
-(define remaining_lines cdr)
-(define current_line car) ;for the parse tree
-
 #|
 
-m_state should return booleans or values to the interpret
+m_state should return a state
 
 |#
 
-;m_state parses each statement and runs them through functions and states and handles returning something to the interpreter
-;1: end of parsedtree was reached but no return was found 2: return was found, so we return the value of the expression after return 3: operator is either a var or =, go to declaring and assigning function
+;m_state parses each statement and runs them through functions and states and handles returning the state to the interpreter
 (define m_state
   (lambda (parsedtree s)
     (cond
@@ -80,17 +73,19 @@ m_state should return booleans or values to the interpret
       ((member? (stmttype parsedtree) ifwhileop) (m_state (cdr parsedtree) (m_state_ifwhile (car parsedtree) s))))))
 
 
-;Return the argument that comes after return
-;TODO handling booleans
+;Assigns 'return variable to a value/boolean in state and then returns the state
 (define m_return
   (lambda (expr s)
     (cond
-      ((null? expr) '())
-      ((number? expr) expr)
-      ((boolean? expr) expr)
-      ((and (pair? expr) (member? (prefix (firstarg expr)) arithop)) (m_value expr s))
-      ((and (pair? expr) (member? (prefix (firstarg expr)) boolop)) (m_bool expr s))
-      (else (m_value (firstarg expr) s)))))
+      ((not (null? (s_find 'return s))) s)
+      ((number? (firstarg expr)) (s_add 'return (firstarg expr) s))
+      ((eq? 'true (firstarg expr)) (s_add 'return (firstarg expr) s))
+      ((eq? 'false (firstarg expr)) (s_add 'return (firstarg expr) s))
+      ((and (pair? (firstarg expr)) (member? (prefix (firstarg expr)) arithop)) (s_add 'return (m_value (firstarg expr) s) s))
+      ((and (pair? (firstarg expr)) (member? (prefix (firstarg expr)) boolop)) (s_add 'return (m_bool (firstarg expr) s) s))
+      ((boolean? (s_find (firstarg expr) s)) (s_add 'return (s_find (firstarg expr) s) s))
+      (else (s_add 'return (s_find (firstarg expr) s) s)))))
+
 
 #|
 
@@ -109,7 +104,7 @@ Functions that should return values
       ((number? expr) expr)
       ((and (not (pair? expr)) (s_isinstate? expr s)) (s_find expr s))
       ((and (not (pair? expr)) (not (s_isinstate? expr s))) (error 'attemptingtooperateonundeclared))
-      ((and (eq? '- (operator expr)) (not (secondary? expr))) (- 0 (m_value (operand1 expr) s)))
+      ((and (eq? '- (operator expr)) (null? (operand2? expr))) (- 0 (m_value (operand1 expr) s)))
       ((eq? '+ (operator expr)) (+ (m_value (operand1 expr) s) (m_value (operand2 expr) s)))
       ((eq? '- (operator expr)) (- (m_value (operand1 expr) s) (m_value (operand2 expr) s)))
       ((eq? '* (operator expr)) (* (m_value (operand1 expr) s) (m_value (operand2 expr) s)))
@@ -121,6 +116,8 @@ Functions that should return values
 (define operand1 cadr)
 ;The operand2 is the second argument
 (define operand2 caddr)
+;The operand2? checks if there is a second argument to the unary operator -
+(define operand2? cddr)
 ;The operator is the operator
 (define operator car)
 ;The condition in an if or while statement
@@ -128,8 +125,9 @@ Functions that should return values
 ;The body of an if or while statement
 (define body (lambda (lis) (cons (caddr lis) '())))
 ;The else clause of an if statment
-(define elsebody (lambda (lis) (car (cdddr lis))))
+(define elsebody (lambda (lis) (cdddr lis)))
 
+;Checks what form of syntax between ifelse and if
 (define isIfElse
   (lambda (lis)
     (cond
@@ -156,10 +154,10 @@ Functions that should return values
 (define m_state_ifwhile
   (lambda (expr s)
     (cond
-      ((eq? (operator expr) 'while) (if (m_bool (execcond expr) s) (m_state_ifwhile expr (m_state (body expr) s)) s))
-      ((isIfElse expr) (if (m_bool (execcond expr) s) (m_state (body expr) s) (m_state (elsebody expr) s)))    ; todo the and clause         todo also change the "variable-like" use of (define **** '())
-      ((isIf expr) ; do it more with just the if body
-      (else s))))); return 
+      ((eq? (operator expr) 'while) (if (m_bool (execcond expr) s) (m_state_ifwhile expr (m_state  (body expr) s)) s))
+      ((isIfElse expr) (if (m_bool (execcond expr) s) (m_state (body expr) s) (m_state (elsebody expr) s)))
+      ((isIf expr) (if (m_bool (execcond expr) s) (m_state (body expr) s) s))
+      (else s)))); return 
 
 #|
 
@@ -168,17 +166,23 @@ Functions that should return an updated state
 |#
 
 ;Declare and/or assign a value by mapping a variable and value to the state
-;1: for attempts to apply var to the same variable error 2: for attempts to simply declare a variable, we map it to the null list '() and add to state 3: for attempts to declare and assign at the same time we take both arguments
-;4: for attempts to reassign a variable we take both arguments and equal 5: should not make it here error
+;1: for attempts to apply var to the same variable error 2: for attempts to simply declare a variable, we map it to the null list '() and add to state
+;3: Declare a variable and determine if it is initalized to a boolean expression 4: Declare a variable and determine if it is initalized to a boolean
+;5: for attempts to declare and assign at the same time we take both arguments 6: Error if variable is undeclared and an attempt to assign is done
+;7: for attempts to reassign a variable to a boolean expression 8: for attempts to reassign a variable to a value expression 5: should not make it here error
 (define s_declassign
   (lambda (expr s)
     (cond
       ((and (eq? (prefix expr) 'var) (s_isinstate? (firstarg expr) s)) (error 'cannotdeclaretwice))
       ((and (eq? (prefix expr) 'var) (null? (secondarg? expr))) (s_add (firstarg expr) uninitializedvalue s))
+      ((and (eq? (prefix expr) 'var) (pair? (secondarg expr)) (member? (prefix (secondarg expr)) boolop)) (s_add (firstarg expr) (m_bool (secondarg expr) s) s))
+      ((and (eq? (prefix expr) 'var) (or (eq? (secondarg expr) 'true) (eq? (secondarg expr) 'false))) (s_add (firstarg expr) (m_bool (secondarg expr) s) s))
       ((eq? (prefix expr) 'var) (s_add (firstarg expr) (m_value (secondarg expr) s) s))
       ((and (eq? (prefix expr) '=) (not (s_isinstate? (firstarg expr) s))) (error 'attemptingtoassigntoundeclared))
+      ((and (eq? (prefix expr) '=) (pair? (secondarg expr)) (member? (prefix (secondarg expr)) boolop)) (s_add (firstarg expr) (m_bool (secondarg expr) s) s))
       ((and (eq? (prefix expr) '=) (s_isinstate? (firstarg expr) s)) (s_add (firstarg expr) (m_value (secondarg expr) s) s))
       (else (error 'unexpectederror)))))
+
 
 ;Helper function that adds a variable to the state, possibly with a value. Also handles updating by removing a variable if it is already in the state and then reading it.
 (define s_add
@@ -186,6 +190,10 @@ Functions that should return an updated state
     (cond
       ((s_isinstate? var s) (s_add var val (s_remove var s)))
       ((number? val) (cons (cons var (varsof s)) (list (cons val (valuesof s)))))
+      ((eq? #t val) (cons (cons var (varsof s)) (list (cons 'true (valuesof s)))))
+      ((eq? #f val) (cons (cons var (varsof s)) (list (cons 'false (valuesof s)))))
+      ((eq? 'true val) (cons (cons var (varsof s)) (list (cons 'true (valuesof s)))))
+      ((eq? 'false val) (cons (cons var (varsof s)) (list (cons 'false (valuesof s)))))
       (else (cons (cons var (varsof s)) (list (cons (m_value val s) (valuesof s))))))))
 
 ;Removes variable and its associated value
@@ -194,18 +202,19 @@ Functions that should return an updated state
     (cond
       ((not (s_isinstate? var s)) (error 'varnotfound))
       ((eq? var (currentvar s)) (cons (restofvars s) (list (restofvalues s))))
-      (else (cons (cons (currentvar s) (varsof (s_remove var (cons (restofvars s) (list (restofvalues s)))))) (list (cons (currentval s) (valsof (s_remove var (cons (restofvalues s) (list (restofvalues s))))))))))))
+      (else (cons (cons (currentvar s) (varsof (s_remove var (cons (restofvars s) (list (restofvalues s)))))) (list (cons (currentvalue s) (valuesof (s_remove var (cons (restofvars s) (list (restofvalues s))))))))))))
+
 
 ;find finds a value that is paired to a variable in state which is a (list list)
-;1: state is empty 2: var list is empty 3: check first variable in var list, return value if it is the same 4: iterate through both lists of state and check again
+;1: state is empty 2: var list is empty 3: check if value is uninitalized 4: check first variable in var list, return value if it is the same 5: iterate through both lists of state and check again
 (define s_find
   (lambda (var s)
     (cond
       ((null? s) (error 'stateisnottwolists))
-      ((null? (varsof s)) (error 'varlistisempty))
+      ((null? (varsof s)) '())
       ((and (eq? (currentvar s) var) (null? (currentvalue s))) (error 'valueisuninitalized))
       ((eq? (currentvar s) var) (currentvalue s))
-      (else (s_find var (cons (restofvars s)) (list (restofvalues s)))))))
+      (else (s_find var (cons (restofvars s) (list (restofvalues s))))))))
 
 ;Get the current variable to be looked at in the state
 (define currentvar caar)
@@ -227,18 +236,23 @@ Functions that should return a boolean
         #f)))
 
 #|
-Consolidated
+
+Boolean function that returns booleans and helper function
+
 |#
 
-
+;Checks for boolean and boolean expressions and evaluates them
 (define m_bool
   (lambda (condition s)
     (cond
       ((null? condition) (error 'invalidarguments))
+      ((eq? condition 'true) #t)
+      ((eq? condition 'false) #f)
+      ((s_isinstate? condition s) (s_find condition s))
       ((not (list? condition)) ;this is if just an atom is passed in
        (if (boolean? condition);checks if the atom is a boolean if so return it
            condition
-           (error 'unexpectederror);error
+            (error 'unexpectederror);error
         ))
       ;not_op: check the not case
       ((eq? (operator condition) '!) (not (m_bool(operand1 condition) s)))
@@ -257,6 +271,7 @@ Consolidated
       ((eq? '< (operator condition)) (comparison_op condition s))
       (else (error 'unexpectederror)))))
 
+;Evaluates comparison operators
 (define comparison_op
   (lambda (expr s)
     ;comparison operators continued
@@ -270,35 +285,36 @@ Consolidated
           (else #f))))
 
 
-  ; == case for m_bool function
-  (define equals_equals
-    (lambda (expr s)
-      (cond
-        ;checks if operands need further evaluation 
-        ((or(list? (operand1 expr)) (list? (operand2 expr)))
-         (if (or(check_arithmetic (operand1 expr)) (check_arithmetic (operand2 expr)))
-             (= (m_value (operand1 expr) s) (m_value (operand2 expr) s))
-             (eqv? (m_bool(operand1 expr) s) (m_bool (operand2 expr) s))))
-         (else (eq? (m_value (operand1 expr) s) (m_value (operand2 expr) s))))))
+; == case for m_bool function
+(define equals_equals
+  (lambda (expr s)
+    (cond
+      ;checks if operands need further evaluation 
+      ((or(list? (operand1 expr)) (list? (operand2 expr)))
+       (if (or(check_arithmetic (operand1 expr)) (check_arithmetic (operand2 expr)))
+           (= (m_value (operand1 expr) s) (m_value (operand2 expr) s))
+           (eqv? (m_bool(operand1 expr) s) (m_bool (operand2 expr) s))))
+      (else (eq? (m_value (operand1 expr) s) (m_value (operand2 expr) s))))))
          
        
-  ;!= case for m_bool function
-  (define not_equals
-    (lambda (expr s)
-      (cond
-        ((or(list? (operand1 expr)) (list? (operand2 expr)))
-         (if (or(check_arithmetic (operand1 expr)) (check_arithmetic (operand2 expr)))
-             (not (eqv? (m_value (operand1 expr) s) (m_value (operand2 expr) s)))
-             (not (eq? (m_bool(operand1 expr) s) (m_bool(operand2 expr) s)))))
-         (else (not(eq? (m_value (operand1 expr) s) (m_value (operand2 expr) s)))))))
+;!= case for m_bool function
+(define not_equals
+  (lambda (expr s)
+    (cond
+      ((or(list? (operand1 expr)) (list? (operand2 expr)))
+       (if (or(check_arithmetic (operand1 expr)) (check_arithmetic (operand2 expr)))
+           (not (eqv? (m_value (operand1 expr) s) (m_value (operand2 expr) s)))
+           (not (eq? (m_bool(operand1 expr) s) (m_bool(operand2 expr) s)))))
+      (else (not(eq? (m_value (operand1 expr) s) (m_value (operand2 expr) s)))))))
 
+;&& case for m_bool function
 (define and_and
   (lambda (expr s)
     (if (not(null? (cddr expr)))
         (and (m_bool (operand1 expr) s) (m_bool (operand2 expr) s))
         (error 'invalidsyntax))))
         
-        
+;|| case for m_bool function
 (define or_or
   (lambda (expr s)
     (if (not (null? (cddr expr)))
@@ -309,19 +325,6 @@ Consolidated
 ;checks if the given expression is an arithmetic expression
 (define check_arithmetic
   (lambda (expr)
-    (if (or (eq? (car expr) '*) (eq? (car expr) '+) (eq? (car expr) '-) (eq? (car expr) '/))
+    (if (or (eq? (operator expr) '*) (eq? (operator expr) '+) (eq? (operator expr) '-) (eq? (operator expr) '/) (eq? (operator expr) '%))
         #t
         #f)))
-
-
-
-
-
-
-
-;This is a simple way to check the parse tree of a textname ONLY FOR TESTING PURPOSES
-;function not implemented however, we can check for example a text file called "example.txt"
-;Included files are simpleparser.scm, lex.scm, this.rkt, and example.txt, three lines were uncommented in lex, two lines were uncommented in simpleparser, one line was commented in lex from the original sources
-(define check
-  (lambda (filename)
-    (parser filename)))
