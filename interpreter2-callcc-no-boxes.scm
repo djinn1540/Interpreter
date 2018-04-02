0bdfd6469f9c2d64573cb60f3d954cabf90a30c1
unknown <twl23@case.edu>
2018-04-02 01:25:25 -0400

    tests 10,14,15,16,19,20 not working - new funcall operator/makes more sense

--- interpreter2-callcc-no-boxes.scm
+++ interpreter2-callcc-no-boxes.scm
@@ -213,7 +213,7 @@
       ((number? expr) expr)
       ((eq? expr 'true) #t)
       ((eq? expr 'false) #f)
-      ((func-exists? expr environment) (func-lookup expr environment))
+      ((and (list? expr) (eq? 'funcall (operator expr))) (interpret-funcall expr environment throw))
       ((not (list? expr)) (lookup expr environment))
       (else (eval-operator expr environment throw)))))
 
@@ -244,7 +244,6 @@
       ((eq? '>= (operator expr)) (>= op1value (eval-expression (operand2 expr) environment throw)))
       ((eq? '|| (operator expr)) (or op1value (eval-expression (operand2 expr) environment throw)))
       ((eq? '&& (operator expr)) (and op1value (eval-expression (operand2 expr) environment throw)))
-      ((eq? 'funcall (operator expr)) (interpret-funcall expr environment throw))
       (else (myerror "Unknown operator:" (operator expr))))))
 
 ; Determines if two values are equal.  We need a special test because there are both boolean and integer types.
